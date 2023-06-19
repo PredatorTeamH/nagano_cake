d07f5fc5d376106a687f834a927086e356f6351d
yusukeee811 <132319174+yusukeee811@users.noreply.github.com>
2023-06-19 12:52:16 +0000

    [Update]新規登録画面

--- db/migrate/20230618054928_devise_create_customers.rb
+++ db/migrate/20230618054928_devise_create_customers.rb
@@ -32,6 +32,13 @@ class DeviseCreateCustomers < ActiveRecord::Migration[6.1]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
+      t.string :last_name,       null:false
+      t.string :first_name,      null:false
+      t.string :last_kana_name,  null:false
+      t.string :first_kana_name, null:false
+      t.string :zip_code,        null:false
+      t.string :address,         null:false
+      t.string :phone_number,    null:false
 
       t.timestamps null: false
     end
