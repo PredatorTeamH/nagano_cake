d43db6290786c67b089c91e58ca39236ace9b92a
yuta-kobayashi-dev <123618181+yuta-kobayashi-dev@users.noreply.github.com>
2023-06-20 04:38:45 +0000

    Add Customers

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,2 +1,49 @@
 class Public::CustomersController < ApplicationController
+  
+  def show
+    # 顧客マイページを表示
+    @customer = Customer.find(params[:id])
+  end
+  
+  def edit
+    # 顧客情報編集ページを表示
+    @customer = Customer.find(params[:id])
+  end
+  
+  def update
+    if @customer.update(customer_params)
+      redirect_to customer_path(current_customer)
+    else
+      render "edit"
+    end
+  end
+  
+  def unsubscribe
+    # 顧客を特定する処理
+    @customer = Cutomer.find(params[:id])
+    # 退会確認画面を表示するための処理
+    render "unsubscibe"
+  end
+  
+  def withdraw
+    @customer = Cutomer.find(params[:id])
+    if @customer.update(status: 'not_active')
+      # 退会が成功した場合の処理
+      # 例えば、成功メッセージを表示したり、リダイレクトしたりする
+      flash[:success] = '退会処理が完了しました。'
+      redirect_to root_path
+    else
+      # 退会が失敗した場合の処理
+      # 例えば、エラーメッセージを表示したり、再度退会画面を表示したりする
+      flash.now[:error] = '退会処理に失敗しました。'
+      render  "unsubscribe" # 退会確認画面を再表示
+    end
+  end
+    
+  private
+  
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :last_kana_name,
+    :first_kana_name, :zip_code, :address, :phone_number, :email)
+  end
 end
