0389d87146974cf6db56fa8b33dbed339f23985d
EC2 Default User <ec2-user@ip-172-31-36-34.ap-northeast-1.compute.internal>
2023-06-20 05:50:42 +0000

    first commit

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -7,4 +7,14 @@ class Customer < ApplicationRecord
   end
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+  validates :last_name, length: { minimum:2, maximum:20 }, uniqueness: true
+  validates :first_name, length: { minimum:2, maximum:20 }, uniqueness: true
+  validates :last_kana_name, length: { minimum:2, maximum:20 }, uniqueness: true
+  validates :first_kana_name, length: { minimum:2, maximum:20 }, uniqueness: true
+  validates :zip_code, uniqueness: true
+  validates :address, uniqueness: true
+  validates :phone_number, uniqueness: true
+  validates :email, uniqueness: true
+  validates :encypted_password, uniqueness: true
 end
