4f6b6c66f51e4330c421e3ea0e77b4f9cfda00bf
sato0w0 <130539468+sato0w0@users.noreply.github.com>
2023-06-21 08:03:24 +0000

    [Update]管理者の商品追加周りの更新

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,13 +1,13 @@
 class Admin::GenresController < ApplicationController
+  def index
+    @genre = Genre.new
+    @genres = Genre.all
+  end
+
   def create
     genre = Genre.new(genre_params)
     genre.save
     redirect_to admin_genres_path
-  end
-
-  def index
-    @genre = Genre.new
-    @genres = Genre.all
   end
 
   def edit
--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,2 +1,3 @@
 class Public::CartItemsController < ApplicationController
+
 end
