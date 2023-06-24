6719f28b6ac333e3de817cbe2630022eab9b1a88
EC2 Default User <ec2-user@ip-172-31-36-34.ap-northeast-1.compute.internal>
2023-06-24 08:08:40 +0000

    注文情報関係

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,15 +1,15 @@
 class Public::CustomersController < ApplicationController
-
+  
   def show
     # 顧客マイページを表示
     @customer = current_customer
   end
-
+  
   def edit
     # 顧客情報編集ページを表示
     @customer = current_customer
   end
-
+  
   def update
     @customer = current_customer
     if @customer.update(customer_params)
@@ -19,31 +19,31 @@ class Public::CustomersController < ApplicationController
       render "edit"
     end
   end
-
+  
   def unsubscribe
     # 顧客を特定する処理
     @customer = current_customer
     # 退会確認画面を表示するための処理
-
+    render "unsubscibe"
   end
-
+  
   def withdraw
-    customer = current_customer
-    customer.is_active = false
-    if customer.save
-      session.clear
+    @customer = current_customer
+    if @customer.update(status: 'not_active')
       # 退会が成功した場合の処理
+      # 例えば、成功メッセージを表示したり、リダイレクトしたりする
       flash[:success] = '退会処理が完了しました。'
       redirect_to root_path
     else
       # 退会が失敗した場合の処理
+      # 例えば、エラーメッセージを表示したり、再度退会画面を表示したりする
       flash.now[:error] = '退会処理に失敗しました。'
-      render 'unsubscribe' # 退会確認画面を再表示
+      render  "unsubscribe" # 退会確認画面を再表示
     end
   end
-
+    
   private
-
+  
   def customer_params
     params.require(:customer).permit(:last_name, :first_name, :last_kana_name,
     :first_kana_name, :zip_code, :address, :phone_number, :email)
