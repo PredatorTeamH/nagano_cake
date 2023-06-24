3eb1aecf562fbc90b3f011d02fd381fb62d72b19
yuta-kobayashi-dev <123618181+yuta-kobayashi-dev@users.noreply.github.com>
2023-06-24 08:09:29 +0000

    Add AdmitCustomer追加

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,29 +1,32 @@
 class Admin::CustomersController < ApplicationController
-  
+  before_action :authenticate_admin!
+
   def index
-    @customers = Customers.all
+    @customers = Customer.page(params[:page])
   end
-  
+
   def show
     @customer =Customer.find(params[:id])
   end
-  
+
   def edit
     @customer =Customer.find(params[:id])
   end
-  
+
   def update
     @customer =Customer.find(params[:id])
-    if customer.update
-      redirect_to customers_path
+    if customer.update(admin_customer_params)
+      flash[:notice] = "登録情報を更新しました"
+      redirect_to admin_customer_path
     else
-      render 'edit'
+      render ：edit
     end
   end
-  
+
   private
   def customer_params
     params.require(:customer).permit(:last_name, :first_name, :last_kana_name,
     :first_kana_name, :zip_code, :address, :phone_number, :email)
   end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -108,10 +108,10 @@ ActiveRecord::Schema.define(version: 2023_06_20_000229) do
 
   create_table "orders", force: :cascade do |t|
     t.integer "customer_id", null: false
-    t.string "shipping_zip_code", null: false
+    t.integer "shipping_zip_code", null: false
     t.string "shipping_address", null: false
     t.string "address_name", null: false
-    t.string "payment_method", null: false
+    t.string "payment_methods", null: false
     t.integer "freight", null: false
     t.integer "total_price", null: false
     t.integer "status", default: 0, null: false
