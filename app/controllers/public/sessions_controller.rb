f6d11a0a402f2297a2fa78dabf309093c493afa9
yuta-kobayashi-dev <123618181+yuta-kobayashi-dev@users.noreply.github.com>
2023-06-26 03:09:06 +0000

    退会処理修正・レイアウト補正

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Public::SessionsController < Devise::SessionsController
+  before_action :customer_state, only: [:create]
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
@@ -18,7 +19,26 @@ class Public::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  # protected
+  protected
+  # 退会しているかを判断するメソッド
+  def customer_state
+    ## 【処理内容1】 入力されたemailからアカウントを1件取得
+    @customer = Customer.find_by(email: params[:customer][:email])
+    ## アカウントを取得できなかった場合、このメソッドを終了する
+    return if !@customer
+    ## 【処理内容2】 取得したアカウントのパスワードと入力されたパスワードが一致してるかを判別
+    if @customer && @customer.valid_password?(params[:customer][:password])
+      ## 【処理内容3】
+      if @customer.is_active?
+        sign_in(@customer)
+        flash[:notice] = "ログインしました。"
+        redirect_to root_path
+      else
+        flash[:danger] = "退会済みのアカウントです。ログインできません。"
+        redirect_to new_customer_registration_path
+      end
+    end
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params
